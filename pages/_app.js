eca665af6b8569475347c39d504fd3a6762300ec
vitaotechx-prog <vitaotechx@gmail.com>
2025-08-06 19:35:20 -0300

    import vercel analytics _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '../Layout';
 import '@/styles/globals.css';
 import { AuthProvider } from '../contexts/AuthContext';
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }) {
 
      {/* O Toaster fica aqui para exibir as notificações */}
      <Toaster />
+     <Analytics />
     </AuthProvider>
   );
 }
